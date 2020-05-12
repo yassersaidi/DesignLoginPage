af46e446340ac5d4e44c9231268b96be9a9cd385
YasserDZ <39775379+yassersaidi@users.noreply.github.com>
2020-05-12 03:45:44 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 
+var check = 0;
+
+passViewBtn.onclick = function () {
+    if(check == 0){
+        pass.setAttribute("type" , "text");
+        passEye.classList.remove("fa-eye");
+        passEye.classList.add("fa-eye-slash");
+        check = 1
+    }else{
+        pass.setAttribute("type" , "password");
+        passEye.classList.remove("fa-eye-slash");
+        passEye.classList.add("fa-eye");
+        check = 0
+    }
+}
